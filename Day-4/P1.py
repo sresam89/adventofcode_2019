80138aa1163456d220b8bdc8d4a6fe33d08e0dd9
Sreeram <sresam8(@gmail.com>
2019-12-08 17:28:28 -0600

    adding missing logic for repetition check

--- Day-4/P1.py
+++ Day-4/P1.py
@@ -16,22 +16,32 @@ while i <= max_value:
     list = split_to_list(i)
     # checking each number at every index with one below for lesser or equal value
     for k in range(1, max_len):
+        # actual digit check
         if list[k-1] > list[k]:
             to_match = list[k-1]
+            # getting digit position
             position = 10**(max_len-1-k)
             # if the previous digit is lesser match the digit to the current value
             i = (i-(i % (position*10))) + (to_match*position)+(i % position)
-            list = split_to_list(i)
+            # doing a split for next iteration in same loop
+            if i < max_value:
+                list = split_to_list(i)
+            else:
+                break
         # keep doing the above till the max_len of the digit is reached
         """ not breaking here; since there could be potential password match in the same number
             for example 138888, 138889, 138899....
+            If the loop reaches max_len limit that mean the program found a matching password
         """
         if k == max_len-1:
-            print(i)
-            password_count += 1
-    # else:
-    #     continue
+            """ adding missing logic for repetition check"""
+            for j in range(1, max_len):
+                if list[j] == list[j-1]:
+                    print(i)
+                    password_count += 1
+                    break
+            else:
+                continue
+            break
     i += 1
-    # break
-
 print("password count", password_count)
